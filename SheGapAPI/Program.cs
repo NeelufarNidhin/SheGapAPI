0a8de85cd47235271c5f60b726d46547c7d0a852
neelufarnidhin <nilu.nidhin@gmail.com>
2024-02-08 05:16:09 +0400

    Initial Commit

---
+++ SheGapAPI/Program.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using SheGapAPI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureLoggerService();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
